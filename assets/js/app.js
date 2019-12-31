b2cf9701089328212c8a6ef4635fd92245a5f1e0
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-30 20:36:31 -0700

    added html for temperature

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,29 +7,51 @@ var queryUrlBase = 'http://api.openweathermap.org/data/2.5/weather?q='
 
 $(document).ready(function() {
 
-
-    
-    function runQuery(queryURL) {
+    // Ajax call for the current weather
+    function currentWeather(queryURL) {
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response)
-        })
+           
+
+            $('#main-card').empty()
+
+            // Create city name <h3> and append to card
+            var cityTitle = $('<h3>');
+            cityTitle.addClass('card-title');
+            cityTitle.text(response.name)
+            $('#main-card').append(cityTitle)
+
+            // convert the temperature to farenheit
+            var temperature = (response.main.temp - 273.15) * 1.80 + 32; 
+            
+            var currentTemp = $('<p>');
+            currentTemp.addClass('temperature');
+            var t = temperature.toString()
+            var temp = t.slice(0,4)
+            currentTemp.html(`Temperature: ${temp}  &#176; F`);
+            cityTitle.append(currentTemp)
+            
+            console.log(temp)
+
+        });
     }
 
 
 
 
-
+// click event that will trigger the ajax call for current and 5 day forecast
     $('#searchBtn').on('click', function(e) {
         e.preventDefault()
         var city = $('#search-city').val().trim()
         console.log(city)
-
+        
         var newUrl = queryUrlBase + city + authKey;
 
-        runQuery(newUrl);
+        // Sends the ajax call the newly assembled url for the current forecast
+        currentWeather(newUrl);
    
     });
 });
